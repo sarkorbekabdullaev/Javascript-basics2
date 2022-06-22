484f2ff346f9bf403dd446e5c77b3b2ca60b2977
Sarkorbek Abdullaev <sarkorbek7777@gmail.com>
2022-06-22 11:57:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var num1 = +prompt(' 1raqam kiriting ');
+var num2 = +prompt(' 2raqam kiriting ');
+console.log(num1 + num2);
+
+
+var name = prompt('ismingiz');
+console.log("meningn ismim " + name);
+
+var age = prompt('yoshingiz');
+console.log("yoshim " + age + " da.");
+
+
+var num1 = +prompt('6+3=?');
+console.log("togri javob 6+3= " + (6 + 3) + "sizning javobingiz " + num1);
